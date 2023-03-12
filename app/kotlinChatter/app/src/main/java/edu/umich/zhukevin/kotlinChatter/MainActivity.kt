2de3b1fe2d98b05585054d02e30eec002b851a02
Kevin Zhu <kevin691827@gmail.com>
2023-03-12 12:44:50 -0400

    Camera button progress

--- app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
+++ app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
@@ -1,12 +1,23 @@
 package edu.umich.zhukevin.kotlinChatter
 
+import android.content.ComponentName
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableList
+import androidx.lifecycle.ViewModel
 import edu.umich.zhukevin.kotlinChatter.ChattStore.chatts
 import edu.umich.zhukevin.kotlinChatter.ChattStore.getChatts
 import edu.umich.zhukevin.kotlinChatter.databinding.ActivityMainBinding
@@ -14,6 +25,9 @@ import edu.umich.zhukevin.kotlinChatter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var view: ActivityMainBinding
     private lateinit var chattListAdapter: ChattListAdapter
+
+    private val viewState: HistoryViewState by viewModels()
+    private lateinit var forCropResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +41,20 @@ class MainActivity : AppCompatActivity() {
         // setup refreshContainer here later
         view.refreshContainer.setOnRefreshListener {
             getChatts()
+        }
+
+        val cropIntent = initCropIntent()
+        var takePicture = registerForActivityResult(ActivityResultContracts.TakePicture())
+        { success ->
+            if (success) {
+                doCrop(cropIntent)
+            } else {
+                Log.d("TakePicture", "failed")
+            }
+        }
+        view.postButton.setOnClickListener {
+            viewState.imageUri = mediaStoreAlloc(mediaType="image/jpeg")
+            takePicture.launch(viewState.imageUri)
         }
 
         refreshTimeline()
@@ -60,9 +88,72 @@ class MainActivity : AppCompatActivity() {
         override fun onItemRangeRemoved(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
     }
 
+    //    crop stuff
+    private fun initCropIntent(): Intent? {
+        // Is there any registered Activity on device to do image cropping?
+        val intent = Intent("com.android.camera.action.CROP")
+        intent.type = "image/*"
+        val listofCroppers = packageManager.queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(0L))
+
+        // No image cropping Activity registered
+        if (listofCroppers.size == 0) {
+            toast("Device does not support image cropping")
+            return null
+        }
+
+        intent.component = ComponentName(
+            listofCroppers[0].activityInfo.packageName,
+            listofCroppers[0].activityInfo.name)
+
+        // create a square crop box:
+        intent.putExtra("outputX", 500)
+            .putExtra("outputY", 500)
+            .putExtra("aspectX", 1)
+            .putExtra("aspectY", 1)
+            // enable zoom and crop
+            .putExtra("scale", true)
+            .putExtra("crop", true)
+            .putExtra("return-data", true)
+
+        return intent
+    }
+
+    private fun mediaStoreAlloc(mediaType: String): Uri? {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mediaType)
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+
+        return contentResolver.insert(
+            if (mediaType.contains("video"))
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            else
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values)
+    }
+
+    private fun doCrop(intent: Intent?) {
+        intent ?: run {
+//            viewState.imageUri?.let { view.previewImage.display(it) }
+            return
+        }
+
+        viewState.imageUri?.let {
+            intent.data = it
+            forCropResult.launch(intent)
+        }
+    }
+
+
     override fun onDestroy() {
         super.onDestroy()
 
         chatts.removeOnListChangedCallback(propertyObserver)
     }
+}
+
+class HistoryViewState: ViewModel() {
+    var enableSend = true
+    var imageUri: Uri? = null
+//    var videoUri: Uri? = null
+//    var videoIcon = android.R.drawable.presence_video_online
 }
