9b959c94183bd0405e898fe09830a3ce4fbebaa9
Kevin Zhu <kevin691827@gmail.com>
2023-03-14 17:08:14 -0400

    Camera button now works, but doesn't save pic yet

--- app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
+++ app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
@@ -1,6 +1,6 @@
 package edu.umich.zhukevin.kotlinChatter
 
-import android.content.ComponentName
+import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -12,7 +12,6 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.ObservableArrayList
@@ -25,9 +24,7 @@ import edu.umich.zhukevin.kotlinChatter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var view: ActivityMainBinding
     private lateinit var chattListAdapter: ChattListAdapter
-
-    private val viewState: HistoryViewState by viewModels()
-    private lateinit var forCropResult: ActivityResultLauncher<Intent>
+    private val viewState: MainViewState by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,26 +40,45 @@ class MainActivity : AppCompatActivity() {
             getChatts()
         }
 
-        val cropIntent = initCropIntent()
+        refreshTimeline()
+        chatts.addOnListChangedCallback(propertyObserver)
+
+        // register camera contract
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            results.forEach {
+                if (!it.value) {
+                    toast("${it.key} access denied")
+                    finish()
+                }
+            }
+        }.launch(arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_MEDIA_IMAGES))
+
+        // error message for no camera
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            toast("Device has no camera!")
+            return
+        }
+
+        //
         var takePicture = registerForActivityResult(ActivityResultContracts.TakePicture())
         { success ->
             if (success) {
-                doCrop(cropIntent)
+                // doCrop(cropIntent)
             } else {
                 Log.d("TakePicture", "failed")
             }
         }
-        view.postButton.setOnClickListener {
+        view.cameraButton.setOnClickListener {
             viewState.imageUri = mediaStoreAlloc(mediaType="image/jpeg")
             takePicture.launch(viewState.imageUri)
         }
-
-        refreshTimeline()
-        chatts.addOnListChangedCallback(propertyObserver)
     }
 
     fun startPost(view: View?) = startActivity(Intent(this, PostActivity::class.java))
 
+    // refresh content
     private fun refreshTimeline() {
         getChatts()
 
@@ -88,36 +104,13 @@ class MainActivity : AppCompatActivity() {
         override fun onItemRangeRemoved(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
     }
 
-    //    crop stuff
-    private fun initCropIntent(): Intent? {
-        // Is there any registered Activity on device to do image cropping?
-        val intent = Intent("com.android.camera.action.CROP")
-        intent.type = "image/*"
-        val listofCroppers = packageManager.queryIntentActivities(intent, PackageManager.ResolveInfoFlags.of(0L))
+    override fun onDestroy() {
+        super.onDestroy()
 
-        // No image cropping Activity registered
-        if (listofCroppers.size == 0) {
-            toast("Device does not support image cropping")
-            return null
-        }
-
-        intent.component = ComponentName(
-            listofCroppers[0].activityInfo.packageName,
-            listofCroppers[0].activityInfo.name)
-
-        // create a square crop box:
-        intent.putExtra("outputX", 500)
-            .putExtra("outputY", 500)
-            .putExtra("aspectX", 1)
-            .putExtra("aspectY", 1)
-            // enable zoom and crop
-            .putExtra("scale", true)
-            .putExtra("crop", true)
-            .putExtra("return-data", true)
-
-        return intent
+        chatts.removeOnListChangedCallback(propertyObserver)
     }
 
+    // store camera image
     private fun mediaStoreAlloc(mediaType: String): Uri? {
         val values = ContentValues()
         values.put(MediaStore.MediaColumns.MIME_TYPE, mediaType)
@@ -130,30 +123,9 @@ class MainActivity : AppCompatActivity() {
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             values)
     }
-
-    private fun doCrop(intent: Intent?) {
-        intent ?: run {
-//            viewState.imageUri?.let { view.previewImage.display(it) }
-            return
-        }
-
-        viewState.imageUri?.let {
-            intent.data = it
-            forCropResult.launch(intent)
-        }
-    }
-
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        chatts.removeOnListChangedCallback(propertyObserver)
-    }
 }
 
-class HistoryViewState: ViewModel() {
+class MainViewState: ViewModel() {
     var enableSend = true
     var imageUri: Uri? = null
-//    var videoUri: Uri? = null
-//    var videoIcon = android.R.drawable.presence_video_online
 }
