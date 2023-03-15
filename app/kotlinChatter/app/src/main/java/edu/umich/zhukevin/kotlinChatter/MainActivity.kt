d07ab8b2110d4990c4826cdc6f00fc88ce57e7c6
Stefan Grueneis <stefang@umich.edu>
2023-03-15 19:55:38 -0400

    first step

--- app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
+++ app/kotlinChatter/app/src/main/java/edu/umich/zhukevin/kotlinChatter/MainActivity.kt
@@ -1,12 +1,22 @@
 package edu.umich.zhukevin.kotlinChatter
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableList
+import androidx.lifecycle.ViewModel
 import edu.umich.zhukevin.kotlinChatter.ChattStore.chatts
 import edu.umich.zhukevin.kotlinChatter.ChattStore.getChatts
 import edu.umich.zhukevin.kotlinChatter.databinding.ActivityMainBinding
@@ -14,6 +24,7 @@ import edu.umich.zhukevin.kotlinChatter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var view: ActivityMainBinding
     private lateinit var chattListAdapter: ChattListAdapter
+    private val viewState: MainViewState by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +42,43 @@ class MainActivity : AppCompatActivity() {
 
         refreshTimeline()
         chatts.addOnListChangedCallback(propertyObserver)
+
+        // register camera contract
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+            results.forEach {
+                if (!it.value) {
+                    toast("${it.key} access denied")
+                    finish()
+                }
+            }
+        }.launch(arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_MEDIA_IMAGES))
+
+        // error message for no camera
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            toast("Device has no camera!")
+            return
+        }
+
+        //
+        var takePicture = registerForActivityResult(ActivityResultContracts.TakePicture())
+        { success ->
+            if (success) {
+                // doCrop(cropIntent)
+            } else {
+                Log.d("TakePicture", "failed")
+            }
+        }
+        view.cameraButton.setOnClickListener {
+            viewState.imageUri = mediaStoreAlloc(mediaType="image/jpeg")
+            takePicture.launch(viewState.imageUri)
+        }
     }
 
     fun startPost(view: View?) = startActivity(Intent(this, PostActivity::class.java))
 
+    // refresh content
     private fun refreshTimeline() {
         getChatts()
 
@@ -65,4 +109,23 @@ class MainActivity : AppCompatActivity() {
 
         chatts.removeOnListChangedCallback(propertyObserver)
     }
+
+    // store camera image
+    private fun mediaStoreAlloc(mediaType: String): Uri? {
+        val values = ContentValues()
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mediaType)
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+
+        return contentResolver.insert(
+            if (mediaType.contains("video"))
+                MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+            else
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values)
+    }
+}
+
+class MainViewState: ViewModel() {
+    var enableSend = true
+    var imageUri: Uri? = null
 }
