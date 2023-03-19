8ae31f7889bcd612fb5fe7838e6b72ee40afbc3a
ngaia01 <ngaia@umich.edu>
2023-03-19 13:54:32 -0400

    Added deletepuzzle to urls.py and views.py

--- puzzled/routing/urls.py
+++ puzzled/routing/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('postchatt/', views.postchatt, name='postchatt'),
     path('getimages/', views.getimages, name='getimages'),
     path('postimages/', views.postimages, name='postimages'),
+    path('deletepuzzle/', views.deletepuzzle, name='deletepuzzle'),
 ]
